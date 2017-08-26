b9b0e8b094127ce7d3217ecfd66345db5cedf4d0
丸山将大 <knoveasel@gmail.com>
2017-08-26 15:28:24 +0900

    first commit

---
+++ wordpress/wp-content/plugins/mm-test/mm-test.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: mm-test
+Plugin URI: http://www.example.com/plugin
+Description: テスト用プラグイン
+Author: masahiro maruyama
+Version: 0.1
+Author URI: http://www.example.com
+*/
+function get_test()
+{
+
+}
